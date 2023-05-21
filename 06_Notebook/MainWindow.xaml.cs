cf4bd398dea9ad3dacade0a0adcbf096124e8d49
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-05-21 15:05:02 +0300

    Relay Command

--- 06_Notebook/MainWindow.xaml.cs
+++ 06_Notebook/MainWindow.xaml.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -28,47 +24,19 @@ namespace _06_Notebook
             this.DataContext = model;   
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            model.deleteSelectedContact();
-        }
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    model.deleteSelectedContact();
+        //}
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            model.DeleteAllContact();
-        }
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    model.DeleteAllContact();
+        //}
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            model.DublicateSelectedContact();
-        }
-    }
-    class ViewModel
-    {
-        private ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
-        public IEnumerable<Contact> Contacts => contacts;
-        public Contact SelectedContact { get; set; }
-        public ViewModel()
-        {
-            contacts.Add(new Contact() { Name = "Vova", Age = 30, Surname = "Pupkin", Phone = "+3807575828", IsMale = true });
-            contacts.Add(new Contact() { Name = "Marusia", Age = 25, Surname = "Shishik", Phone = "+380771244", IsMale = false });
-            contacts.Add(new Contact() { Name = "Olga", Age = 33, Surname = "Shelesh", Phone = "+38067285792", IsMale = false });
-
-        }
-        public void deleteSelectedContact()
-        {   
-            if(SelectedContact != null)
-                contacts.Remove(SelectedContact);   
-        }
-        public void DublicateSelectedContact()
-        {
-            if (SelectedContact != null)
-                contacts.Add(SelectedContact.Clone());
-        }
-        public void DeleteAllContact()
-        {
-            if (contacts.Any())
-                contacts.Clear();
-        }
+        //private void Button_Click_2(object sender, RoutedEventArgs e)
+        //{
+        //    model.DublicateSelectedContact();
+        //}
     }
 }
---
+++ 07_file_viewer/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Printing.IndexedProperties;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace _07_file_viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel viewModel = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult res = dialog.ShowDialog();    
+
+            if(res == CommonFileDialogResult.Ok)
+            {
+                //MessageBox.Show(dialog.FileName);
+                viewModel.LoadFiles(dialog.FileName);
+            }   
+           
+        }
+    }
+    class ViewModel :INotifyPropertyChanged
+    {
+        private ObservableCollection<FileInfo> files = new ObservableCollection<FileInfo>();
+        public IEnumerable<FileInfo> Files => files;
+        private string directoryPath;
+        public string DirectoryPath { get=> directoryPath; 
+            set {
+                directoryPath = value;
+                OnPropertyChanged(); OnPropertyChanged(nameof(DirectoryName));
+            } 
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }       
+        public string DirectoryName => DirectoryPath;
+        private FileInfo selectedfile;
+        public FileInfo SelectedFile 
+        { get=>selectedfile; 
+            set { selectedfile = value;OnPropertyChanged(); } 
+        }
+        public ViewModel()
+        {
+            LoadFiles(@"C:\Users\helen\Desktop\Заняття");
+        }
+        public void LoadFiles(string dirPath)
+        {
+            this.DirectoryPath = dirPath;
+            DirectoryInfo directory = new DirectoryInfo(dirPath);
+            var data =  directory.GetFiles();
+
+            files.Clear();
+            foreach (var item in data)
+            {
+                files.Add(item);
+            }
+        }
+    }
+}
